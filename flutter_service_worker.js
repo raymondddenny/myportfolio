65035ec0ab6d57d1c996a45e1c9ee0de656a4752
raymondddenny <dennyraymondd@gmail.com>
2020-05-25 20:47:46 +0800

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,9 +1,11 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "8230d9f2a719d3361838b44cd9b399be",
+  "assets/AssetManifest.json": "d9c4f96b9a787988a6a87abd31097484",
 "assets/assets/images/edit.png": "8a8b9c48937be90a26de3f9107a82e63",
 "assets/assets/images/id.svg": "1cfe41605519ecf1c766a6abfe02239b",
+"assets/assets/images/intro.flr": "3ec5cd241c72ef914a76be34056fa23e",
+"assets/assets/images/profile.png": "eb3bc3263dae1d5a27b1e37ae05a6e62",
 "assets/FontManifest.json": "3db24aa684e79108ca2b33d760b8e321",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/fonts/MyFlutterApp.ttf": "6f87773e4a127e853cfce7b299e449cd",
@@ -13,7 +15,7 @@ const RESOURCES = {
 "icons/android-icon-192x192.png": "a61ecfa63f25c8ff9010710b466f63db",
 "index.html": "8e7da37924894a29eb4670717b72f0ba",
 "/": "8e7da37924894a29eb4670717b72f0ba",
-"main.dart.js": "ee0f77bd723333004bb94609afa9a9a3",
+"main.dart.js": "d12467b33e4e5cec798c6352a2defb21",
 "manifest.json": "02f19b9c46f4dc9c170d126367dd7d77",
 "README.md": "1c1d5b185090dd97bbc23ba2451a124d"
 };
